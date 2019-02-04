83338ab0d277d2f0a22975f5443004231086c7f8
harshsingh54 <46954928+harshsingh54@users.noreply.github.com>
2019-02-04 11:13:38 +0530

    Made change in branch

--- SampleProject/src/com/telstra/sample/SampleClass.java
+++ SampleProject/src/com/telstra/sample/SampleClass.java
@@ -5,6 +5,8 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add a line of code");
+		
+		System.out.println("Change added in branch");
 	}
 
 }
