dc2bb107e4052ce366a234507cb0de80e89259bf
Harsh Vardhan Singh <harsh.vardhan.singh38@gmail.com>
2019-02-04 10:55:12 +0530

    Added a new Java Project

---
+++ SampleProject/src/com/telstra/sample/SampleClass.java
@@ -0,0 +1,10 @@
+package com.telstra.sample;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Add a line of code");
+	}
+
+}
